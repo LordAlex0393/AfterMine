2e62464aabba11fd1e3573744f09f62bab686d60
LordAlex0393 <tntgameboomlive@gmail.com>
2023-09-10 23:19:21 +0700

    SecondCommit

--- src/main/java/com/lordalex/aftermine/Main.java
+++ src/main/java/com/lordalex/aftermine/Main.java
@@ -22,7 +22,7 @@ public class Main {
 	
 	public static final String MODID = "Aftermine";
 	public static final String MODNAME = "AfterMine";
-	public static final String VERSION = "1.1.0";
+	public static final String VERSION = "1.1.1";
 	
 	public static Block strongCobblestone;
 	public static Item strongCobblestoneSword;
