e0a2ea2e0867f63d74605bec5fac266b62a4fc79
Home_Loader <alexstone0390@gmail.com>
2023-09-12 20:43:59 +0700

    StairsUpdateFix

--- src/main/java/com/lordalex/aftermine/Main.java
+++ src/main/java/com/lordalex/aftermine/Main.java
@@ -22,7 +22,7 @@ public class Main {
 
 	public static final String MODID = "Aftermine";
 	public static final String MODNAME = "AfterMine";
-	public static final String VERSION = "1.1.3";
+	public static final String VERSION = "1.1.4";
 
 	public static Block strongCobblestone;
 	public static Block strongStoneBricks;
